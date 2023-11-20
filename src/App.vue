30c641d5b63b31874090bb19e628b9b2bf78c20b
Thales Davi de Souza <thalesdavisoul@gmail.com>
2023-11-20 09:05:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import DexHeader from './components/DexHeader.vue';
+import DexBanner from './components/DexBanner.vue';
+</script>
+
+<template>
+  <DexHeader />
+  <DexBanner />
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<!-- TODO: Fazer um about com os membros do grupo -->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+</style>
